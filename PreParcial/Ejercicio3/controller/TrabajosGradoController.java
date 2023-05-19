cff21afb788b6bad20d2738fb9936f4f7cffc446
unknown <huendycaicedo@gmail.comg>
2023-05-18 23:32:31 -0500

    ventana y controller

--- PreParcial/Ejercicio3/controller/TrabajosGradoController.java
+++ PreParcial/Ejercicio3/controller/TrabajosGradoController.java
@@ -1,4 +1,51 @@
 package Ejercicio3.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class TrabajosGradoController {
+
+    @FXML
+    private TableColumn<?, ?> apellidoAutor;
+
+    @FXML
+    private TableView<?> autoresTable;
+
+    @FXML
+    private TableColumn<?, ?> cedulaAutor;
+
+    @FXML
+    private TableColumn<?, ?> codigoTrabajo;
+
+    @FXML
+    private TableColumn<?, ?> descripcionTrabajo;
+
+    @FXML
+    private TableColumn<?, ?> fechaTrabajo;
+
+    @FXML
+    private TableColumn<?, ?> nombreAutor;
+
+    @FXML
+    private Button pedirServicio;
+
+    @FXML
+    private TableColumn<?, ?> programaAutor;
+
+    @FXML
+    private TableView<?> tableTrabajos;
+
+    @FXML
+    private TableColumn<?, ?> tituloProfesionalAutor;
+
+    @FXML
+    private TableColumn<?, ?> tituloTrabajo;
+
+    @FXML
+    void pedirServicioAction(ActionEvent event) {
+
+    }
 }
